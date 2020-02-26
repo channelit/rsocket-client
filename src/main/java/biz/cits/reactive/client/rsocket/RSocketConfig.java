76340d03feb447c0629ed712525dfd0d30edea7b
Hardik <hardikgw@gmail.com>
2020-02-26 05:50:26 -0500

    init

---
+++ src/main/java/biz/cits/reactive/client/rsocket/RSocketConfig.java
@@ -0,0 +1,56 @@
+package biz.cits.reactive.client.rsocket;
+
+import io.rsocket.RSocket;
+import io.rsocket.RSocketFactory;
+import io.rsocket.frame.decoder.PayloadDecoder;
+import io.rsocket.transport.netty.client.TcpClientTransport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.codec.CharSequenceEncoder;
+import org.springframework.core.codec.StringDecoder;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
+import org.springframework.messaging.rsocket.MetadataExtractor;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+import org.springframework.util.MimeTypeUtils;
+
+@Configuration
+public class RSocketConfig {
+
+    @Autowired
+    private RSocketRequester.Builder builder;
+
+
+    @Bean
+    RSocket rSocket() {
+        return RSocketFactory
+                .connect()
+                .dataMimeType(MimeTypeUtils.APPLICATION_JSON_VALUE)
+                .frameDecoder(PayloadDecoder.ZERO_COPY)
+                .transport(TcpClientTransport.create("localhost", 7000))
+                .start()
+                .block();
+    }
+
+//    @Bean
+//    RSocketStrategies rsocketStrategies() {
+//        return RSocketStrategies.builder()
+//                .decoder(StringDecoder.allMimeTypes())
+//                .encoder(CharSequenceEncoder.allMimeTypes())
+//                .dataBufferFactory(new DefaultDataBufferFactory(true))
+//                .build();
+//    }
+
+    @Bean
+    RSocketRequester rSocketRequester(RSocketStrategies strategies) {
+        return RSocketRequester.builder()
+                .rsocketFactory(factory -> factory
+                        .dataMimeType(MimeTypeUtils.ALL_VALUE)
+                        .frameDecoder(PayloadDecoder.ZERO_COPY))
+                .rsocketStrategies(strategies)
+                .connect(TcpClientTransport.create("localhost", 7000))
+                .retry().block();
+    }
+
+}
---
+++ src/main/java/biz/cits/reactive/client/rsocket/RSocketController.java
@@ -0,0 +1,28 @@
+package biz.cits.reactive.client.rsocket;
+
+import biz.cits.reactive.model.Message;
+import org.reactivestreams.Publisher;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RSocketController {
+
+    private final RSocketRequester rSocketRequester;
+
+    public RSocketController(RSocketRequester rSocketRequester) {
+        this.rSocketRequester = rSocketRequester;
+    }
+
+    @GetMapping(value = "/messages/{filter}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Publisher<Message> getMessages(@PathVariable String filter) {
+        return rSocketRequester
+                .route("messages")
+                .data(new Message(filter))
+                .retrieveFlux(Message.class);
+    }
+
+}
