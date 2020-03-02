9398d30a9fea4d337dacc139414e3970fc631251
Hardik <hardikgw@gmail.com>
2020-03-02 05:34:00 -0500

    publisher

--- src/main/java/biz/cits/reactive/client/rsocket/RSocketController.java
+++ src/main/java/biz/cits/reactive/client/rsocket/RSocketController.java
@@ -1,12 +1,15 @@
 package biz.cits.reactive.client.rsocket;
 
 import biz.cits.reactive.model.Message;
+import biz.cits.reactive.model.MsgGenerator;
 import org.reactivestreams.Publisher;
 import org.springframework.http.MediaType;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class RSocketController {
@@ -31,6 +34,21 @@ public class RSocketController {
                 .route("camel/" + filter)
                 .data(filter)
                 .retrieveFlux(Message.class);
+    }
+
+    @GetMapping(value = "/post", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Mono<String> getMessages(@RequestParam int numMessages) {
+        Message m = new Message(MsgGenerator.getMessages(1).get(0).getValue());
+        System.out.println(m);
+        return rSocketRequester
+                .route("post")
+                .data(m).retrieveMono(String.class);
+//        MsgGenerator.getMessages(numMessages).forEach(m -> {
+//            System.out.println(m);
+//            rSocketRequester
+//                    .route("post/me")
+//                    .data(m).send().block();
+//        });
     }
 
 }
