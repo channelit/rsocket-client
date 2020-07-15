db8d9ebc2dd8e90ec97cdbfcb3d038c273f3a132
Hardik <hardikgw@gmail.com>
2020-07-15 10:42:52 -0400

    stream post

--- src/main/java/biz/cits/reactive/client/rsocket/RSocketController.java
+++ src/main/java/biz/cits/reactive/client/rsocket/RSocketController.java
@@ -1,6 +1,5 @@
 package biz.cits.reactive.client.rsocket;
 
-import autovalue.shaded.com.google$.common.collect.$TreeRangeSet;
 import biz.cits.reactive.model.ClientMessage;
 import biz.cits.reactive.model.Message;
 import biz.cits.reactive.model.MsgGenerator;
@@ -10,11 +9,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.CompositeByteBuf;
 import io.rsocket.Payload;
 import io.rsocket.RSocket;
 import io.rsocket.core.RSocketConnector;
 import io.rsocket.core.Resume;
+import io.rsocket.exceptions.RejectedResumeException;
 import io.rsocket.frame.decoder.PayloadDecoder;
+import io.rsocket.metadata.CompositeMetadataCodec;
+import io.rsocket.metadata.RoutingMetadata;
+import io.rsocket.metadata.TaggingMetadataCodec;
 import io.rsocket.metadata.WellKnownMimeType;
 import io.rsocket.resume.InMemoryResumableFramesStore;
 import io.rsocket.transport.netty.client.TcpClientTransport;
@@ -29,27 +35,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.BufferOverflowStrategy;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 import reactor.util.retry.Retry;
 
+import java.net.ConnectException;
 import java.nio.channels.ClosedChannelException;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.Executors;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @RestController
 @Order(1)
 public class RSocketController {
 
+    private final CompositeByteBuf metadataPosts;
     private Mono<RSocket> rSocket;
+    private RSocket rSocketPublisher;
     private final RSocketRequester rSocketRequester;
+    CompositeByteBuf metadataPost;
     private ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule()).configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 
     Logger logger = LoggerFactory.getLogger(RSocketController.class);
@@ -57,14 +64,19 @@ public class RSocketController {
 
     public RSocketController(RSocket rSocket, RSocketRequester rSocketRequester) throws InterruptedException {
         s = Schedulers.newSingle("inspect");
-        s.schedulePeriodically(this::inspect, 0, 5, TimeUnit.SECONDS);
+        s.schedulePeriodically(this::inspect, 0, 15, TimeUnit.SECONDS);
         s.start();
         init();
         this.rSocketRequester = rSocketRequester;
-//        String filter = "ABCDE";
-//        String data = "select message FROM messages WHERE (message->>'messageDateTime')::timestamp with time zone > '2020-04-27 09:19:58.89'::timestamp without time zone";
-//        long i = 0;
-//        RSocket mySocket = this.rSocket.block();
+        String filter = "ABCDE";
+        String data = "select message FROM messages WHERE (message->>'messageDateTime')::timestamp with time zone > '2020-04-27 09:19:58.89'::timestamp without time zone";
+        long i = 0;
+//        RSocket mySocket = RSocketConnector.create()
+//                .payloadDecoder(PayloadDecoder.ZERO_COPY)
+//                .dataMimeType(WellKnownMimeType.APPLICATION_CBOR.toString())
+//                .metadataMimeType(WellKnownMimeType.APPLICATION_CBOR.getString())
+//                .connect(TcpClientTransport.create("localhost", 7000)).cache().block();
+
 //        while (i < 16000) {
 //            i++;
 //            data = "{\"id\":\"" + UUID.randomUUID().toString() + "\"}";
@@ -74,10 +86,23 @@ public class RSocketController {
 //            message.put("filter", filter);
 //            message.put("data", data);
 //            System.out.println(i);
-//            mySocket.requestStream(DefaultPayload.create(message.toString()).touch()).retry(5).subscribe();
+//            mySocket.requestStream(DefaultPayload.create(message.toString())).doOnError(this::handleConnectionError).retry().subscribe();
 //        }
+        
+        this.metadataPost = getRouteMetadata("post/me");
+        this.metadataPosts = getRouteMetadata("posts/me");
+
     }
 
+    private CompositeByteBuf getRouteMetadata(String route) {
+        CompositeByteBuf metadata = ByteBufAllocator.DEFAULT.compositeBuffer();
+        RoutingMetadata routingMetadata = TaggingMetadataCodec.createRoutingMetadata(ByteBufAllocator.DEFAULT, List.of(route));
+        CompositeMetadataCodec.encodeAndAddMetadata(metadata,
+                ByteBufAllocator.DEFAULT,
+                WellKnownMimeType.MESSAGE_RSOCKET_ROUTING,
+                routingMetadata.getContent());
+        return metadata;
+    }
     private void inspect() {
         ObjectNode message = mapper.createObjectNode();
         message.put("route", "inspect");
@@ -85,17 +110,17 @@ public class RSocketController {
         message.put("filter", "");
         message.put("data", "");
         Flux<Payload> s = rSocket.flatMapMany(requester ->
-                requester.requestStream(DefaultPayload.create(message.toString())));
+                requester.requestStream(DefaultPayload.create(message.toString()))).doOnError(this::handleConnectionError).retry(1);
         s.map(this::logThread).map(Payload::getDataUtf8).subscribe(msg-> logger.info(msg));
     }
 
     private void init() {
         Resume resume =
                 new Resume()
-                        .sessionDuration(Duration.ofSeconds(1000))
-                        .storeFactory(t -> new InMemoryResumableFramesStore("client", 500_000_000))
+                        .sessionDuration(Duration.ofDays(10))
+                        .storeFactory(t -> new InMemoryResumableFramesStore("client", 500_000))
                         .cleanupStoreOnKeepAlive()
-                        .retry(Retry.fixedDelay(Long.MAX_VALUE, Duration.ofSeconds(1))
+                        .retry(Retry.fixedDelay(Long.MAX_VALUE, Duration.ofSeconds(5))
                                 .doBeforeRetry(
                                         retrySignal -> {
                                             System.out.println("Disconnected. Trying to resume connection...");
@@ -112,27 +137,30 @@ public class RSocketController {
                 RSocketConnector.create()
                         .reconnect(Retry.fixedDelay(100, Duration.ofSeconds(5)))
                         .resume(resume)
-                        .keepAlive(Duration.ofSeconds(1),Duration.ofHours(100))
+                        .keepAlive(Duration.ofSeconds(1),Duration.ofDays(2))
                         .payloadDecoder(PayloadDecoder.ZERO_COPY)
                         .dataMimeType(WellKnownMimeType.APPLICATION_CBOR.toString())
-                        .metadataMimeType(WellKnownMimeType.APPLICATION_CBOR.getString())
-                        .connect(TcpClientTransport.create("localhost", 7000));
+                        .metadataMimeType(WellKnownMimeType.MESSAGE_RSOCKET_COMPOSITE_METADATA.getString())
+                        .connect(TcpClientTransport.create("localhost", 7000))
+                        .retryWhen(Retry.backoff(Long.MAX_VALUE, Duration.ofSeconds(1)))
+                        .cache();
     }
 
-    @GetMapping(value = "/socket/{route}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Publisher<String> socket(@PathVariable String route) {
+    @GetMapping(value = "/socket/{client}/{route}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Publisher<String> socket(@PathVariable String client, @PathVariable String route) {
         String filter = "ABCDE";
         String data = "select message FROM messages WHERE (message->>'messageDateTime')::timestamp with time zone > '2020-04-27 09:19:58.89'::timestamp without time zone";
-        data = "{\"id\":'id'}";
+        data = "{\"id\":\"id\"}";
+//        data = "";
         ObjectNode message = mapper.createObjectNode();
         message.put("route", route);
-        message.put("client", "me");
+        message.put("client", client);
         message.put("filter", filter);
         message.put("data", data);
         Flux<Payload> s = rSocket.flatMapMany(requester ->
                 requester.requestStream(DefaultPayload.create(message.toString()))
-        ).doOnError(this::handleConnectionError).retry();
-        return s.map(this::logThread).map(Payload::getDataUtf8);
+        ).doOnError(this::handleConnectionError).retry(1);
+        return s.map(Payload::getDataUtf8);
     }
 
     @GetMapping(value = "/socket/bulk-post", produces = MediaType.TEXT_PLAIN_VALUE)
@@ -140,7 +168,7 @@ public class RSocketController {
         String filter = "ABCDE";
         String data = "select message FROM messages WHERE (message->>'messageDateTime')::timestamp with time zone > '2020-04-27 09:19:58.89'::timestamp without time zone";
         long i = 0;
-        while (i < 3000) {
+        while (i < 50000) {
             i++;
             data = "{\"id\":\"" + UUID.randomUUID().toString() + "\"}";
             ObjectNode message = mapper.createObjectNode();
@@ -151,12 +179,40 @@ public class RSocketController {
 
             System.out.println(i);
 
-            Flux<Payload> s = rSocket.flatMapMany(requester ->
-                    requester.requestStream(DefaultPayload.create(message.toString()))
-            ).doOnError(this::handleConnectionError).retry();
+            ByteBuf byteBufData = ByteBufAllocator.DEFAULT.buffer().writeBytes(data.getBytes());
+            Mono<Payload> s = rSocket.flatMap(requester ->
+                    requester.requestResponse(DefaultPayload.create(byteBufData, metadataPost))
+            ).doOnError(this::handleConnectionError).retry(1).log();
 
-            s.subscribe();
+            s.map(Payload::getDataUtf8).map(this::checkResult).subscribe(System.out::println);
 //            rSocketPublisher.requestStream(DefaultPayload.create(message.toString())).doOnError(this::handleConnectionError).retry().subscribe();
+        }
+        return Flux.just("ok");
+    }
+
+    @GetMapping(value = "/socket/bulk-stream", produces = MediaType.TEXT_PLAIN_VALUE)
+    public Publisher<String> socketBulkStream() {
+        String filter = "ABCDE";
+        String data = "select message FROM messages WHERE (message->>'messageDateTime')::timestamp with time zone > '2020-04-27 09:19:58.89'::timestamp without time zone";
+        long i = 0;
+        List<String> messages = new ArrayList<>();
+        while (i < 50000) {
+            i++;
+            data = "{\"id\":\"" + UUID.randomUUID().toString() + "\"}";
+            messages.add(data);
+            System.out.println(i);
+
+            if (i % 1000 == 0) {
+                Flux<Payload> payloads = Flux.create(payloadFluxSink -> {
+                    messages.forEach(
+                            message -> payloadFluxSink.next(DefaultPayload.create(ByteBufAllocator.DEFAULT.buffer().writeBytes(message.getBytes()), metadataPosts))
+                    );
+                });
+                Flux<Payload> s = rSocket.flatMapMany(requester ->
+                        requester.requestChannel(payloads)
+                ).doOnError(this::handleConnectionError).retry(1).log();
+                s.map(Payload::getDataUtf8).map(this::checkResult).subscribe(System.out::println);
+            }
         }
         return Flux.just("ok");
     }
@@ -167,9 +223,12 @@ public class RSocketController {
     }
 
     private synchronized void handleConnectionError(Throwable throwable) {
-        if (throwable instanceof ClosedChannelException) {
+        if (throwable instanceof ClosedChannelException || throwable instanceof RejectedResumeException || throwable instanceof ConnectException || throwable instanceof IllegalStateException) {
             init();
             System.out.println("Closed Channel Exception Occurred In Subscriber");
+        } else {
+            logger.error(throwable.getMessage());
+            throwable.printStackTrace();
         }
     }
 
